ea8e3948b577cbfaa606f21f477f87be9da0a8d4
Vitalij35 <kostenkovitalij35@gmail.com>
2025-06-16 17:55:53 +0300

    Cloudflare warp cli added to packages list

--- Builder/creators/daemons.py
+++ Builder/creators/daemons.py
@@ -5,11 +5,16 @@ class Daemons:
     @staticmethod
     def enable_all_daemons():
         Daemons.__enable_network_daemon()
+        Deamons.__enable_warp_deamon()
         Daemons.__enable_bluetooth_daemon()
 
     @staticmethod
     def __enable_network_daemon():
         os.system("sudo systemctl enable NetworkManager")
+
+    @staticmethod
+    def __enable_warp_deamon():
+        os.system("sudo systemctl enable warp-svc.service")
     
     @staticmethod
     def __enable_bluetooth_daemon():
